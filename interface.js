0ddd92107c63ba7e6736f8d4201304f65bbffed8
Laura Collett <la.lo.collett@gmail.com>
2018-06-26 18:01:52 +0100

    adding reset button

--- interface.js
+++ interface.js
@@ -4,12 +4,18 @@ $(document).ready(() => {
     $('#cards').hide();
     $('#deal-instructions').hide();
     $('#start').on('click', () => {
-        $('#cards').toggle();
+        $('#cards').show();
         $('#deal-instructions').show();
     })
 
     $('#cards').on('click', () => {
         //Game.deal();
+    })
+
+    $('#reset').on('click', () => {
+        $('#cards').hide();
+        $('#deal-instructions').hide();
+        $('#reset').hide();
     })
 
 })
