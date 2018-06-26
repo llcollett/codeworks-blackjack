f62f711cf73f2b0fa2305e20d1cb3ae80cb06db4
Laura Collett <la.lo.collett@gmail.com>
2018-06-26 18:03:09 +0100

    adding reset button

--- interface.js
+++ interface.js
@@ -3,9 +3,11 @@ $(document).ready(() => {
 
     $('#cards').hide();
     $('#deal-instructions').hide();
+    $('#reset').hide();
     $('#start').on('click', () => {
         $('#cards').show();
         $('#deal-instructions').show();
+        $('#reset').show();
     })
 
     $('#cards').on('click', () => {
